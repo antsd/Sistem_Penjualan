6102f3d6ff4d63c82ba9e5b3fe78906b5d6a0c7f
Anastasius <antsdevp@gmail.com>
2021-04-13 10:00:03 +0700

    setup server config model

---
+++ server/app/models/index.js
@@ -0,0 +1,38 @@
+const config = require("../config/db.config");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: config.pool.max,
+    min: config.pool.min,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle,
+  },
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("../models/user.model")(sequelize, Sequelize);
+db.role = require("../models/role.model")(sequelize, Sequelize);
+
+db.role.belongsToMany(db.user, {
+  trough: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "roleId",
+});
+
+db.user.belongsToMany(db, role, {
+  trough: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
+});
+
+db.ROLES = ["user", "admin"];
+
+module.exports = db;
