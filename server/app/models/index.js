0a7a10337563bba52c4a366ee17da8fa937fd6d1
Anastasius <antsdevp@gmail.com>
2021-04-13 19:07:40 +0700

    create client react and login masih ada public, need force to login

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,4 +1,4 @@
-const config = require("../config/db.config");
+const config = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
@@ -15,11 +15,12 @@ const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
 });
 
 const db = {};
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.user = require("../models/user.model")(sequelize, Sequelize);
-db.role = require("../models/role.model")(sequelize, Sequelize);
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
