0b9a8c6b5ff59614b36583afdf53d17c377eb2c7
Anastasius <antsdevp@gmail.com>
2021-04-18 22:06:19 +0700

    pushhh

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-import AuthService from "./services/auth.service";
-
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Profile from "./components/Profile";
-import BoardUser from "./pages/BoardUser";
-import BoardAdmin from "./pages/BoardAdmin";
-import Home from "./pages/Home";
-
-const App = () => {
-  const [showAdminBoard, setShowAdminBoard] = useState(false);
-  const [currentUser, setCurrentUser] = useState(undefined);
-
-  useEffect(() => {
-    const user = AuthService.getCurrentUser();
-
-    if (user) {
-      setCurrentUser(user);
-      setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
-    }
-  }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-  };
-
+function App() {
   return (
-    <div>
-      {currentUser ? (
-        <nav className='navbar navbar-expand navbar-dark bg-dark'>
-          <div className='navbar-nav ml-auto'>
-            <li className='nav-item'>
-              <Link to={"/profile"} className='nav-link'>
-                {currentUser.username}
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <a href='/login' className='nav-link' onClick={logOut}>
-                Log Out
-              </a>
-            </li>
-          </div>
-        </nav>
-      ) : (
-        <div></div>
-      )}
-
-      <div className='container mt-3'>
-        <Switch>
-          <Route exact path={"/"} component={BoardUser} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/profile' component={Profile} />
-          <Route path='/user' component={BoardUser} />
-          <Route path='/admin' component={BoardAdmin} />
-        </Switch>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
